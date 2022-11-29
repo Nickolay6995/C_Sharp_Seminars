8fa2fa35840a1a95bbc526ed22d52c59d499e8ee
Nickolay Kochelaevskiy <Nickolas-dragus@yandex.ru>
2022-11-29 21:04:28 +0300

    Задача 10

---
+++ Seminar_2/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1000 && number > 99)
+{
+    int secondDigit = number / 10 % 10;
+    Console.WriteLine($"Вторая цифра: {secondDigit}");
+}
