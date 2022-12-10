8540288b912bed2a01bdc037c69f2fbb172f2917
Nickolay Kochelaevskiy <Nickolas-dragus@yandex.ru>
2022-12-10 23:40:40 +0300

    Задача 34

---
+++ Seminar_5/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+
+*/
+
+// Инициализирует новый массив
+int[] InitArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+
+    return array;
+}
+
+// Печатает массив на экран
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// Получаем колличество чётных чисел в массиве.
+int GetPositiveNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+
+    return count;
+}
+
+int[] arr = InitArray(4);
+PrintArray(arr);
+int count = GetPositiveNumbers(arr);
+
+Console.WriteLine($"Колличество чётных чисел = {count}");
