56e25cfe0120a4dfa61ccb6da196005a4f56ac59
Nickolay Kochelaevskiy <Nickolas-dragus@yandex.ru>
2022-12-22 19:32:18 +0300

    Задача 60

---
+++ Seminar_8/Task4/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+
+*/
+
+int[,,] GetMatrix(int m, int n, int s)
+{
+    int[,,] matrix = new int[m, n, s];
+    Random rnd = new Random();
+    int count = rnd.Next(10, 100 - (matrix.GetLength(0) + matrix.GetLength(1) + matrix.GetLength(2)));
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = count;
+                count++;
+            }
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+int[,,] matrix = GetMatrix(2, 2, 2);
+PrintMatrix(matrix);
